490059adb4dad83b210ed12ab14d40a89e2324a2
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-05 14:56:12 +0100

    KA-226 Remove unnecessary logic from methods
    Remove logic from methods to be just dummy methods, place static array of notes
    directly into NoteController.

--- NotesBackEnd/src/Notes.Api/Controllers/NoteController.cs
+++ NotesBackEnd/src/Notes.Api/Controllers/NoteController.cs
@@ -1,61 +1,58 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Microsoft.Web.Http;
 using Notes.Api.Model;
 
 namespace Notes.Api.Controllers
 {
+    [ApiVersion("1.0")]
+    [RoutePrefix("api/v{version:apiVersion}/notes")]
     public class NoteController : ApiController
     {
-        private Note[] _notesList = NotesArray.GetInitialNotes();
+        private static readonly Note[] NotesList = {
+            new Note {Text = "First note", Id = new Guid("2c00d1c2-fd2b-4c06-8f2d-130e88f719c2")},
+            new Note {Text = "Second note", Id = new Guid("ebcb3d81-af4e-428f-a22d-e7852d70d3a0")},
+            new Note {Text = "Third note", Id = new Guid("599442c0-ae28-4157-9a3f-0491bb4ba6c1")},
+            new Note {Text = "Fourth note", Id = new Guid("4785546e-824d-42a4-900b-e656f19ffb59")}
+        };
 
         [HttpGet]
+        [Route("find/notes")]
         public async Task<IHttpActionResult> FindNotesAsync()
         {
-            var items = await Task.FromResult(_notesList);
+            var items = await Task.FromResult(NotesList);
             return Ok(items);
         }
 
         [HttpGet]
+        [Route("find/{id}")]
+
         public async Task<IHttpActionResult> FindNoteByIdAsync(Guid id)
         {
-            var foundItem = await Task.FromResult(_notesList[0]);
+            var foundItem = await Task.FromResult(NotesList[0]);
             return Ok(foundItem);
         }
 
         [HttpPost]
+        [Route("add/{text}")]
         public async Task<IHttpActionResult> AddNoteAsync(string text)
         {
-            var newNotesArray = new Note[_notesList.Length + 1];
-            _notesList.CopyTo(newNotesArray, 0);
-            newNotesArray[_notesList.Length] = new Note(text, new Guid("d0a0f5bc-0f46-43f0-ab4b-06860c3ea19c"), false);
-            _notesList = newNotesArray;
-
-            //just for using await in async function - Ok() is also possible
-            return await Task.FromResult(Ok(_notesList));
+            return await Task.FromResult(Created($"/find/{NotesList[1].Id}", NotesList[1]));
         }
 
         [HttpPut]
+        [Route("update/{text}")]
         public async Task<IHttpActionResult> UpdateNoteAsync(Guid id, string text)
         {
-            var noteToUpdate = _notesList.SingleOrDefault(note => note.Id == id);
-            if (noteToUpdate != null)
-            {
-                var index = Array.IndexOf(_notesList, noteToUpdate);
-                noteToUpdate.Text = text;
-                _notesList[index] = noteToUpdate;
-            }
-
-            return await Task.FromResult(Ok(_notesList));
+            return await Task.FromResult(Ok(NotesList[2]));
         }
 
         [HttpDelete]
+        [Route("delete/{id}")]
         public async Task<IHttpActionResult> DeleteNoteByIdAsync(Guid id)
         {
-            _notesList = _notesList.Where(note => note.Id != id).ToArray();
-
-            return await Task.FromResult(Ok(_notesList));
+            return await Task.FromResult(Ok(NotesList[3]));
         }
     }
 }
