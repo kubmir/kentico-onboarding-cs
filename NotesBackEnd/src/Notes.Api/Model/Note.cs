150b0117c78eba2a690e461246f7770bdd961faf
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-05 14:51:08 +0100

    KA-226 Refactor Note

--- NotesBackEnd/src/Notes.Api/Model/Note.cs
+++ NotesBackEnd/src/Notes.Api/Model/Note.cs
@@ -6,18 +6,10 @@ namespace Notes.Api.Model
     {
         public string Text { get; set; }
         public Guid Id { get; set; }
-        public bool IsEditActive { get; set; }
-
-        public Note(string text, Guid id, bool isEditActive)
-        {
-            Text = text;
-            Id = id;
-            IsEditActive = isEditActive;
-        }
 
         public override string ToString()
         {
-            return $"{nameof(Text)}: {Text}, {nameof(Id)}: {Id}, {nameof(IsEditActive)}: {IsEditActive}";
+            return $"{nameof(Text)}: {Text}, {nameof(Id)}: {Id}";
         }
     }
 }
