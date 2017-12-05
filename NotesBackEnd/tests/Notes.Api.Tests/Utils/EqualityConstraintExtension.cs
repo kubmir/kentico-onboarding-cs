a5795cc34c309350810016b25b6a20c43cae793a
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-05 14:50:22 +0100

    KA-226 Extend EqualityConstraint to using NoteEqualityComparer

---
+++ NotesBackEnd/tests/Notes.Api.Tests/Utils/EqualityConstraintExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+
+namespace Notes.Api.Tests.Utils
+{
+    static class EqualityConstraintExtension
+    {
+        public static EqualConstraint UsingNoteComparer(this EqualConstraint equalConstraint) 
+            => equalConstraint.Using(NoteEqualityComparer.Instance);
+    }
+}
