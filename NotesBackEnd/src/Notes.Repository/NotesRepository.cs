5213696f8201326aa4e495485c9744bbd1c75fe6
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-07 11:38:17 +0100

    KA-227 Add NotesRepository initial file

---
+++ NotesBackEnd/src/Notes.Repository/NotesRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Notes.Models.Model;
+using Notes.Models.Repository;
+
+namespace Notes.Repository
+{
+    public class NotesRepository : INotesRepository
+    {
+        public Task<Note> CreateNoteAsync(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> DeleteNoteByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Note>> GetAllNotesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> GetNoteByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> UpdateNote(Note note)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
