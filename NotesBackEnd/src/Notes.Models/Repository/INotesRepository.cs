acc37a9cb12623061ac59369e11799cf0a78d72f
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-07 11:37:16 +0100

    KA-227 Add Models assembly with INotesRepository and Note model

---
+++ NotesBackEnd/src/Notes.Models/Repository/INotesRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Notes.Models.Model;
+
+namespace Notes.Models.Repository
+{
+    public interface INotesRepository
+    {
+        Task<IEnumerable<Note>> GetAllNotesAsync();
+
+        Task<Note> GetNoteByIdAsync(Guid id);
+
+        Task<Note> CreateNoteAsync(Note note);
+
+        Task<Note> DeleteNoteByIdAsync(Guid id);
+
+        Task<Note> UpdateNote(Note note);
+    }
+}
