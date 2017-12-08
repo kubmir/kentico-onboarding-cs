9328d7896fca4bdb2d2cbdd763618956542e710a
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-08 16:28:28 +0100

    KA-227 Refactor NotesControllerTest init

--- NotesBackEnd/tests/Notes.Api.Tests/Controllers/NotesControllerTest.cs
+++ NotesBackEnd/tests/Notes.Api.Tests/Controllers/NotesControllerTest.cs
@@ -17,24 +17,20 @@ namespace Notes.Api.Tests.Controllers
     [TestFixture]
     internal class NotesControllerTest
     {
-        private NotesController _controller;
         private static readonly Note Note1 = new Note {Text = "First note", Id = new Guid("2c00d1c2-fd2b-4c06-8f2d-130e88f719c2")};
         private static readonly Note Note2 = new Note {Text = "Second note", Id = new Guid("ebcb3d81-af4e-428f-a22d-e7852d70d3a0")};
         private static readonly Note Note3 = new Note {Text = "Third note", Id = new Guid("599442c0-ae28-4157-9a3f-0491bb4ba6c1")};
         private static readonly Note Note4 = new Note {Text = "Fourth note", Id = new Guid("4785546e-824d-42a4-900b-e656f19ffb59")};
         private static readonly Note[] AllNotes = {Note1, Note2, Note3, Note4};
 
+        private NotesController _controller;
+
         [SetUp]
         public void Init()
         {
-            var mockedDb = Substitute.For<INotesRepository>();
-            mockedDb.GetAllNotesAsync().Returns(AllNotes);
-            mockedDb.GetNoteByIdAsync(Arg.Any<Guid>()).Returns(Note1);
-            mockedDb.CreateNoteAsync(Arg.Any<Note>()).Returns(Note2);
-            mockedDb.UpdateNoteAsync(Arg.Any<Note>()).Returns(Note3);
-            mockedDb.DeleteNoteByIdAsync(Arg.Any<Guid>()).Returns(Note4);
+            var mockedNotesRepository = MockNotesRepository();
 
-            _controller = new NotesController(mockedDb)
+            _controller = new NotesController(mockedNotesRepository)
             {
                 Configuration = new HttpConfiguration(),
                 Request = new HttpRequestMessage
@@ -131,6 +127,18 @@ namespace Notes.Api.Tests.Controllers
             executedResponse.TryGetContentValue(out T actualContent);
 
             return (actualContent, executedResponse);
+        }
+
+        internal INotesRepository MockNotesRepository()
+        {
+            var mockedRepository = Substitute.For<INotesRepository>();
+            mockedRepository.GetAllNotesAsync().Returns(AllNotes);
+            mockedRepository.GetNoteByIdAsync(Arg.Any<Guid>()).Returns(Note1);
+            mockedRepository.CreateNoteAsync(Arg.Any<Note>()).Returns(Note2);
+            mockedRepository.UpdateNoteAsync(Arg.Any<Note>()).Returns(Note3);
+            mockedRepository.DeleteNoteByIdAsync(Arg.Any<Guid>()).Returns(Note4);
+
+            return mockedRepository;
         }
     }
 }
