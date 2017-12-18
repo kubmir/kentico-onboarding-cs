8507c139c12c29f282371f314f655e7bfb6af579
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-18 15:04:34 +0100

    KA-227 Add interface for DI container

---
+++ NotesBackEnd/src/Notes.Contracts/Dependency/IMyContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Notes.Contracts.Dependency
+{
+    public interface IMyContainer
+    {
+        IMyContainer RegisterType<TFrom>();
+
+        IMyContainer RegisterType<TFrom, TTo>() where TTo : TFrom;
+
+        object Resolve(Type serviceType);
+
+        IEnumerable<object> ResolveAll(Type serviceType);
+
+        IMyContainer CreateChildContainer();
+
+        void Dispose();
+    }
+}
