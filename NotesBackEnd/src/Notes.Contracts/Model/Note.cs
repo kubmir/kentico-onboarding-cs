bba95ecb2919402199226ef6f1fd9d50fb563aeb
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-20 18:27:10 +0100

    KA-228 Extend note model with dates

--- NotesBackEnd/src/Notes.Contracts/Model/Note.cs
+++ NotesBackEnd/src/Notes.Contracts/Model/Note.cs
@@ -7,9 +7,15 @@ namespace Notes.Contracts.Model
         public string Text { get; set; }
         public Guid Id { get; set; }
 
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastModificationDate { get; set; }
+
         public override string ToString()
         {
-            return $"{nameof(Text)}: {Text}, {nameof(Id)}: {Id}";
+            return $"{nameof(Text)}: {Text}, {nameof(Id)}: {Id}, " +
+                   $"{nameof(CreationDate)}: {CreationDate}, " +
+                   $"{nameof(LastModificationDate)}: {LastModificationDate}";
         }
     }
 }
