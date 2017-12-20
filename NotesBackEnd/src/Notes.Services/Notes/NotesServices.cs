a047cb342444d17fcc89f18180d62714a4bbe974
Miroslav Kubus <MiroslavK@kentico.com>
2017-12-20 18:31:30 +0100

    KA-228 Add NotesServices implementation

---
+++ NotesBackEnd/src/Notes.Services/Notes/NotesServices.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Notes.Contracts.Model;
+using Notes.Contracts.Repository;
+using Notes.Contracts.Services.Date;
+using Notes.Contracts.Services.Notes;
+
+namespace Notes.Services.Notes
+{
+    internal class NotesServices : INotesServices
+    {
+        private readonly IDateService _dateService;
+        private readonly INotesRepository _repository;
+
+        public NotesServices(IDateService dateService, INotesRepository repository)
+        {
+            _dateService = dateService;
+            _repository = repository;
+        }
+
+        public async Task<Note> CreateNoteAsync(Note note)
+        {
+            var dateTime = _dateService.GetCurrentDateTime();
+
+            note.CreationDate = dateTime;
+            note.LastModificationDate = dateTime;
+
+            return await _repository.CreateNoteAsync(note);
+        }
+
+        public async Task<Note> GetNoteAsync(Guid id)
+        {
+            return await _repository.GetNoteByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Note>> GetAllNotesAsync()
+        {
+            return await _repository.GetAllNotesAsync();
+        }
+    }
+}
