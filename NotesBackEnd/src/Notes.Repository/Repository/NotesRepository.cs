e44ac0cfb832ac115e1038bc21ddb53c11049812
Miroslav Kubus <MiroslavK@kentico.com>
2018-02-12 17:37:52 +0100

    KA-228 Add update and delete functionality

--- NotesBackEnd/src/Notes.Repository/Repository/NotesRepository.cs
+++ NotesBackEnd/src/Notes.Repository/Repository/NotesRepository.cs
@@ -10,13 +10,6 @@ namespace Notes.Repository.Repository
 {
     internal class NotesRepository : INotesRepository
     {
-        private static readonly Note[] Notes = {
-            new Note {Text = "First note", Id = new Guid("2c00d1c2-fd2b-4c06-8f2d-130e88f719c2")},
-            new Note {Text = "Second note", Id = new Guid("ebcb3d81-af4e-428f-a22d-e7852d70d3a0")},
-            new Note {Text = "Third note", Id = new Guid("599442c0-ae28-4157-9a3f-0491bb4ba6c1")},
-            new Note {Text = "Fourth note", Id = new Guid("4785546e-824d-42a4-900b-e656f19ffb59")}
-        };
-
         private readonly IMongoCollection<Note> _persistedNotes;
 
         public NotesRepository(IDatabaseContext databaseContext)
@@ -37,10 +30,18 @@ namespace Notes.Repository.Repository
             return note;
         }
 
-        public Task<Note> UpdateNoteAsync(Note note)
-            => Task.FromResult(Notes[2]);
+        public async Task<Note> UpdateNoteAsync(Note note)
+        {
+            var filter = Builders<Note>.Filter.Eq("Id", note.Id);
+            var update = Builders<Note>
+                .Update
+                .Set("Text", note.Text)
+                .CurrentDate("LastModificationDate");
 
-        public Task<Note> DeleteNoteByIdAsync(Guid id)
-            => Task.FromResult(Notes[3]);
+            return await _persistedNotes.FindOneAndUpdateAsync(filter, update);
+        }
+
+        public async Task<Note> DeleteNoteByIdAsync(Guid id)
+            => await _persistedNotes.FindOneAndDeleteAsync(note => note.Id == id);
     }
 }
