d0ca7d1d866d677e15f9e6a0ea436a706f8b938c
Miroslav Kubus <MiroslavK@kentico.com>
2018-03-16 11:03:23 +0100

    KA-228 Add tests for GuidService

---
+++ NotesBackEnd/tests/Notes.Services.Tests/StaticWrappers/GuidServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Notes.Contracts.Services.Utils;
+using Notes.Services.StaticWrappers;
+using NUnit.Framework;
+
+namespace Notes.Services.Tests.StaticWrappers
+{
+    internal class GuidServiceTests
+    {
+        private IGuidService _guidService;
+
+        [SetUp]
+        public void SetUp()
+            => _guidService = new GuidService();
+        
+        [Test]
+        public void GenerateGuid_GenerateNewGuid_NonEmptyGuidReturned()
+        {
+            var generatedGuid = _guidService.GetNew();
+
+            Assert.That(generatedGuid, Is.Not.EqualTo(Guid.Empty));
+        }
+
+        [Test]
+        public void GenerateGuid_Generate100Guids_UniqueGuidsReturned()
+        {
+            List<Guid> generatedGuids = new List<Guid>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                generatedGuids.Add(_guidService.GetNew());
+            }
+
+            Assert.That(generatedGuids.Distinct().Count(), Is.EqualTo(generatedGuids.Count));
+        }
+    }
+}
