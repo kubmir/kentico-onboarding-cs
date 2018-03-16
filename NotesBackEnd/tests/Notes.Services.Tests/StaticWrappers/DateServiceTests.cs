c8571c37dbea750c97414ee948c5745d862fc37b
Miroslav Kubus <MiroslavK@kentico.com>
2018-03-16 10:54:47 +0100

    KA-228 Add tests for IDateService

---
+++ NotesBackEnd/tests/Notes.Services.Tests/StaticWrappers/DateServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Notes.Contracts.Services.Utils;
+using Notes.Services.StaticWrappers;
+using NUnit.Framework;
+
+namespace Notes.Services.Tests.StaticWrappers
+{
+    internal class DateServiceTests
+    {
+        private IDateService _dateService;
+
+        [SetUp]
+        public void SetUp()
+            => _dateService = new DateService();
+
+        [Test]
+        public void GetCurrentDateTime_CalledTwice_SecondReturnedTimeBiggerThanFirstReturned()
+        {
+            var firstTime = _dateService.GetCurrentDateTime();
+            var secondTime = _dateService.GetCurrentDateTime();
+
+            Assert.That(secondTime, Is.GreaterThan(firstTime));
+        }
+    }
+}
